e7f338101c454af12bd5a01594ea2f2a052bce90
Minto <xpeed96@gmail.com>
2022-08-22 22:24:13 +0900

    코드수정

---
+++ src/service/auth_service.js
@@ -0,0 +1,8 @@
+import firebase from "firebase/compat";
+
+class AuthService {
+    login(providerName) {
+        const authProvider = new firebase.auth[`${providerName}AuthProvider`];
+        log
+    }
+}
